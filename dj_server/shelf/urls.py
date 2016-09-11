bf70a2d25c5fc507ff3c4cfcb552ee02c191d8d8
Nguyen Nhat Cuong <nguyen.nhat.cuong@gmail.com>
2016-09-12 01:04:22 +0200

    signup login complete functional, css almost done

--- dj_server/shelf/urls.py
+++ dj_server/shelf/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url, include
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
 from django.contrib.auth.views import *
 
-from shelf.views import LendingPageView
+from shelf.views import LendingPageView, SignupView
 
 urlpatterns = [
     url(r'^api/', include('shelf.api.urls')),
@@ -11,10 +9,11 @@ urlpatterns = [
 
     url(r'^$', login, kwargs={'redirect_authenticated_user': True}, name='login'),
     url(r'^logout/', logout_then_login, kwargs={'login_url': '/'}),
-    url(r'^signup/', CreateView.as_view(
-        template_name='registration/signup.html',
-        form_class=UserCreationForm,
-        success_url='/'
-    )),
+    # url(r'^signup/', CreateView.as_view(
+    #     template_name='registration/signup.html',
+    #     form_class=UserCreationForm,
+    #     success_url='/'
+    # ), name='signup'),
+    url(r'^signup/', SignupView.as_view(), name='signup'),
 ]
 
--- dj_server/shelf/views.py
+++ dj_server/shelf/views.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 class LendingPageView(LoginRequiredMixin, TemplateView):
-    login_url = '/login/'
-    template_name = 'lendingpage.html'
+    login_url = '/'
+    template_name = 'lending_page.html'
+
+
+class SignupView(FormView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = '/home'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        all_errors = list(context_data['form'].errors.values())
+        if all_errors:
+            context_data['first_error_msg'] = all_errors[0][0]
+        return context_data
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)
