408102542c3b39fa4b029c71f7501b2ad8b72411
Nguyen Nhat Cuong <nguyen.nhat.cuong@gmail.com>
2016-11-15 21:42:26 +0100

    add allowed hosts

--- dj_server/shelf_project/settings/prod.py
+++ dj_server/shelf_project/settings/prod.py
@@ -2,7 +2,8 @@ from .common import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
-    'booklender.nhatcuong.biz'
+    'booklender.nhatcuong.biz',
+    'booklender.nhatcuong.co'
 ]
 
 BASE_URL = 'http://booklender.nhatcuong.biz'
